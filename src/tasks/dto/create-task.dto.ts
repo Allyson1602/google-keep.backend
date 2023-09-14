f7d02398e3c996b91b2c8388b5cf26698ab045a5
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-14 19:17:52 -0300

    feat: implementação de validação nos atributos das entidades

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,9 +1,23 @@
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsInt()
   id: number;
 
+  @IsInt()
   listing_id: number;
 
+  @IsOptional()
+  @IsString()
   description: string;
 
+  @IsBoolean()
   done: boolean;
 }
