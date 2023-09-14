1f09e5302d07d665fb6016fc3adbd4bfdac573cb
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-14 18:13:15 -0300

    refactor: adição do .env no .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ListingsModule } from './listings/listings.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from './data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ListingsModule, TypeOrmModule.forRoot(DataSource)],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(DataSource),
+    ListingsModule,
+  ],
   controllers: [],
   providers: [],
 })
