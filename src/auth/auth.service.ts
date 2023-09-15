2e25de5d9f95abb8965d39ff4e1cc902130030bb
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-15 10:02:45 -0300

    chore: configuração de endpoint para autenticação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(key: string): Promise<any> {
+    const user = await this.usersService.findOne(key);
+
+    const payload = { sub: user.id, key: user.key };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
